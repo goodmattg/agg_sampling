eebf6bf693b51420f00c2380666c054e08e99b3a
goodmattg <mattgoodman13@gmail.com>
2015-07-04 11:41:24 -0500

    all calcs now using numpy func

--- quandl_req.py
+++ quandl_req.py
@@ -66,51 +66,41 @@ def getValdata(stock_list, data_pt):
             ind_st = stock_list.index(st)
             dat_table[ind_d, ind_st] = data_struct[st][d]
 
-    return [data_struct, unif_dates, dat_table, stock_list]
+    return [unif_dates, dat_table, stock_list]
 
 
-def buildCorr(data_struct):
+def buildCorr(dt_table, st_list):
 
-    st_list = list(data_struct.keys())  # stock list
-    all_data = dict()
+    num_dp = dt_table.shape[0]
+    num_st = dt_table.shape[1]
+    num_dif = num_dp-1
 
     # Instantiate the correlation and covariance matrix
-    corr_mat = np.zeros((len(st_list), len(st_list)))
-    covar_mat = np.zeros((len(st_list), len(st_list)))
+    corr_mat = np.zeros((num_st, num_st))
+    covar_mat = np.zeros((num_st, num_st))
+    perc_table = np.zeros((num_dif, num_st))
+    avg_table = np.zeros(num_st)
 
-    for st in st_list:
-        # List of date:price tuples stock st
-        st_tuples = list(data_struct[st].items())
-        # Most recent dates first
-        st_tuples.sort(key=lambda x: x[0], reverse=True)
-        # List of prices for "st"
-        st_pr = [pr for (date, pr) in st_tuples]
-        # Close percentages of "st"
-        st_dif = [getPerc(cur, prev) for cur, prev in zip(st_pr, st_pr[1:])]
-        # Average close percentage of "st"
-        st_avg = sum(st_dif)/len(st_dif)
-        # Add to storage dict
-        all_data[st] = (st_dif, st_avg, st_pr)
+    for st in range(num_st):
+        pri = dt_table[:, st]
+        st_dif = [getPerc(c, p) for c, p in zip(pri, pri[1:])]
+        perc_table[:, st] = st_dif
+        avg_table[st] = sum(st_dif)/len(st_dif)
 
-    # Signal is most recent set of price differences
-    signal = dict()
-    for k, v in all_data.items():
-        signal[k] = v[0][0]
+    signal = perc_table[0, :]
 
     for a in st_list:
         for b in st_list:
 
-            a_dif, a_avg, _ = all_data[a]
-            b_dif, b_avg, _ = all_data[b]
+            a_ind = st_list.index(a)
+            b_ind = st_list.index(b)
 
-            if(len(a_dif) == len(b_dif)):
-                num_samples = len(a_dif)
-            else:
-                print("CATASTROPHIC ERROR\n Somehow different sized data")
+            a_dif, a_avg = perc_table[:, a_ind], avg_table[a_ind]
+            b_dif, b_avg = perc_table[:, b_ind], avg_table[b_ind]
 
             # Calculate covariance and correlation
             covar = sum((x[0]-a_avg)*(x[1]-b_avg)
-                        for x in zip(a_dif, b_dif))/num_samples
+                        for x in zip(a_dif, b_dif))/num_dif
 
             corr = covar/(np.std(a_dif)*np.std(b_dif))
 
@@ -118,15 +108,18 @@ def buildCorr(data_struct):
             corr_mat[st_list.index(a), st_list.index(b)] = corr
             covar_mat[st_list.index(a), st_list.index(b)] = covar
 
-    return corr_mat, covar_mat, signal
+    return corr_mat, covar_mat, signal, perc_table, dt_table, avg_table
 
 
 if __name__ == '__main__':
-    [dt_struct, dates, dt_table, st_list] = getValdata(['AAPL', 'MYE'], 10)
+    [_, dt_table, st_list] = getValdata(['AAPL', 'MYE', 'GOOGL', 'MSFT'], 15)
 
-    corr_mat, covar_mat, signal = buildCorr(dt_struct)
+    corr_mat, covar_mat, signal, perc_table, dt_table, avg_table = \
+        buildCorr(dt_table, st_list)
 
-    print(covar_mat)
-    print("Covariance matrix built with: {0} data points".format(len(dates)))
-    print(corr_mat)
-    print("Correlation matrix built with: {0} data points".format(len(dates)))
+    pdb.set_trace()
+
+    # print(covar_mat)
+    # print("Covariance matrix built with: {0} data points".format(len(dates)))
+    # print(corr_mat)
+    # print("Correlation matrix built with: {0} data points".format(len(dates)))
